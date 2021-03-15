65162856ac97c6c93fa7d2d284bf1524aabef818
vincent@macbook <vincent@macbook.air>
2021-03-15 22:42:52 +0800

    assets/json

--- web/entrypoint.sh
+++ web/entrypoint.sh
@@ -1,22 +1,11 @@
 #!/bin/sh
 # notes:
 #  -- to share volume with node-gateway
-
-out=$1
-if [ $out ];then
-  if [ -d $out ];then
-      # script entrypoint.sh
-      # 1# mv  ./src .yml to $output
-      # 2# soft link to $output
-      # 3# npm start
-      
-      cp -r /usr/share/web/src/pages $out/
-      cp -r /usr/src/web/src/pages/* $out/pages
-
-      rm -rf /usr/src/web/src/pages
-      ln -s $out/pages /usr/src/web/src/pages
-      
-  fi
+out='/usr/src/web/src/pages'
+if [ ! $(ls $out) ];then
+  echo cp -r /usr/share/web/src/pages/* $out 
+  cp -r /usr/share/web/src/pages/* $out
+  find $out
 fi
 
 # start
